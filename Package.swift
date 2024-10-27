e69f5cc46e63a2fba12c5603e0c892570fd90581
Theo <mail@theo.sh>
2024-10-27 10:57:29 +0100

    Add platforms

--- Package.swift
+++ Package.swift
@@ -5,7 +5,12 @@ import PackageDescription
 
 let package = Package(
     name: "Quilt",
-    platforms: [.macOS(.v12)],
+    platforms: [
+        .iOS(.v15),
+        .macOS(.v12),
+        .tvOS(.v13),
+        .watchOS(.v7),
+    ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
