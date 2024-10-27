54793d3f8dc28be77f0c4cca49ac486d87a16b62
Theo <mail@theo.sh>
2024-10-27 11:35:05 +0100

    Update swift tools

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 5.10
+// swift-tools-version: 6.0
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -33,5 +33,6 @@ let package = Package(
             name: "QuiltTests",
             dependencies: ["Quilt"]
         ),
-    ]
+    ],
+    swiftLanguageModes: [.v6]
 )
